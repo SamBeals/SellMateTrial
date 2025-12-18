45220067dbbfc340aac188b053293ee6ab719d7f
SamBeals <sdb286@nau.edu>
2025-12-18 09:50:14 -0700

    Initial Commit

---
+++ SellMateTrialTests/SellMateTrialTests.swift
@@ -0,0 +1,17 @@
+//
+//  SellMateTrialTests.swift
+//  SellMateTrialTests
+//
+//  Created by Sam on 12/18/25.
+//
+
+import Testing
+@testable import SellMateTrial
+
+struct SellMateTrialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
